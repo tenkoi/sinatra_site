36600e39c988b6ffe7b0eaecae88793f0cee34af
Faiz <tenkois@gmail.com>
2018-11-26 10:49:59 +0800

    establish intial routes

--- helloworld.rb
+++ helloworld.rb
@@ -4,3 +4,12 @@ get '/' do
 
   "Helloworld!"
 end
+
+get '/goodbye' do
+  "Goodbye world"
+end
+
+get '/about' do
+  output = "<h2>Sinatra</h2>"
+  output << "<p>This my first sinatra App</p>"
+end
