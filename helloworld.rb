1180c37c9d1382504549d3dd9b229e7628b8d239
Faiz <tenkois@gmail.com>
2018-11-28 07:18:35 +0800

    done http method

--- helloworld.rb
+++ helloworld.rb
@@ -2,7 +2,11 @@ require 'sinatra'
 
 get '/' do
 
-  "Helloworld!"
+output = "Hello World"
+output << "<br/>"
+output << "<a href=\"/goodbye\">Leave</a>"
+output
+
 end
 
 get '/goodbye' do
@@ -12,4 +16,13 @@ end
 get '/about' do
   output = "<h2>Sinatra</h2>"
   output << "<p>This my first sinatra App</p>"
+end
+
+get '/form' do
+  form ='<form action="/form" method="post">'
+  form << '<input type="submit" value="Submit"/>'
+end
+
+post '/form' do
+  "Thank You for submitting the form to us!"
 end
